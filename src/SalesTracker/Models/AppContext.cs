25e79972dae2a7a135a04d2de50c629a3f700407
Alexandra Holcombe and Jiwon Kang <alexandra+jiwon@>
2017-05-03 10:16:44 -0700

    Create user model, modify dependencies, create AppContext class

---
+++ src/SalesTracker/Models/AppContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesTracker.Models
+{
+    public class AppContext : IdentityDbContext<User>
+    {
+        public AppContext()
+        {
+        }
+
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SalesTracker;integrated security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
